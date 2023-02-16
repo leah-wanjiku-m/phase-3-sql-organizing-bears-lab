b473f09c918fa3f83bc922ffafe9ff01d6c883c6
leah wanjiku <leagm22@gmail.com>
2023-02-16 13:25:53 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Mr. Chocolate', '34','M','brown','loving','true');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Rowdy', '4','F','pink','cute','true');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Tabitha', '51','F','black','rude','false');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Sergeant Brown', '44','M','yellow','sunny','true');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Melissa', '34','F','white','reserved','true');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Grinch', '14','M','black','loud','false');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Wendy', '64','F','yellow','smart','false');
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES (null, '34','M','brown','aggressive','true');
